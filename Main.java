f5ee4414b5f827d9d832f863040aad62894bea68
Buzdin-Vladislav <116458625+Buzdin-Vladislav@users.noreply.github.com>
2023-08-16 19:56:29 +0000

    gfhg

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Working human = new HumanWorker();
+        ToEat humanEating = new HumanWorker();
+        Working robot = new RobotWorker();
+
+        human.work(); // Человек работает
+        humanEating.eat(); // Человек ест
+        robot.work(); // Робот работает
+    }
+}
